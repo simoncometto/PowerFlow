63c3a64e66bc23de2c85953b2f15c8fe87099657
Simón <simon.cometto@gmail.com>
2019-09-26 05:52:51 -0300

    Jacobiano sparse
    Implementación sparse del cálculo del jacobiano

--- jacobian.py
+++ jacobian.py
@@ -1,27 +1,44 @@
 import numpy as np
-import IEEE_cdf as cdf
+from scipy import sparse
 from math import sin, cos
 
-n, mat_admitancia, load, generation, voltage_phase  = cdf.read("ieee14cdf.txt")
-#Vector de 2 veces la cantidad de nodos. Una vez para la tension y otra para la fase
-v_and_theta = np.ones((2*n), dtype=np.float64)
 
-#Jacobiano de P y Q es una matriz de 2n x 2n
-J = np.zeros([2*n,2*n], dtype=np.float64)
+def jacobian(mat_admitancia, v_and_theta, swing_bus):
+    '''
 
-#Obtengo cada posicioón del Jacobiano
-#dP/dtheta
-for i in range(0,n):
-    for j in range(0,n):
-        #Computo de elementos que no son de la diagonal
+    :return: J Jacobiano de P y Q es una matriz de 2(n-1) x 2(n-1)'''
+
+    n, n = mat_admitancia.get_shape()
+    J = sparse.coo_matrix([2*(n-1),2*(n-1)])
+
+    #Se obtiene cada posicioón del Jacobiano iterando sobre el jacobiano
+    # cuando se itera sobre elementos del swing_bus se pasa por alto.
+
+    already_pass_swing_bus = False
+
+    for i, j, Y in zip(mat_admitancia.row, mat_admitancia.col, mat_admitancia.data):
+        #Saltear el swing_bus
+        if (i == swing_bus || j == swing_bus):
+            already_pass_swing_bus = True
+            continue
+
+        i_J = i
+        j_J = j
+
+        if already_pass_swing_bus:
+            i_J = i - 1
+            j_J = j - 1
+
+        #Elementos que no son de la diagonal
+        # ---------------------------------------------------------------------------------------------
         if(i!=j):
-            v_i = v_and_theta[i]
-            v_j = v_and_theta[j]
-            theta_i = v_and_theta[n + i]
-            theta_j = v_and_theta[n + j]
+            v_i = v_and_theta[i, 0]
+            v_j = v_and_theta[j, 0]
+            theta_i = v_and_theta[n, 1]
+            theta_j = v_and_theta[n, 1]
             delta_theta = theta_i - theta_j
-            G_ij = mat_admitancia[i,j].real
-            B_ij = mat_admitancia[i,j].imag
+            G_ij = mat_admitancia[i, j].real
+            B_ij = mat_admitancia[i, j].imag
 
             cos_theta = cos(delta_theta)
             sin_theta = sin(delta_theta)
@@ -30,34 +47,43 @@ for i in range(0,n):
             b = a * G_ij
             c = a * B_ij
             # dP/dtheta
-            J[i, j] = b * sin_theta - c * cos_theta
+            J[i_J, j_J] = b * sin_theta - c * cos_theta
             # dQ/dtheta
-            J[n + i, j] = -b * cos_theta + c * sin_theta
-
+            J[(n - 1) + i_J, j_J] = -b * cos_theta + c * sin_theta
 
             d = v_i * G_ij
             e = v_i * B_ij
-            #dP/dV
-            J[i,n+j] =  d * cos(delta_theta) + e * sin(delta_theta)
-            #dQ/dV
-            J[n+i, n+j] = d * sin(delta_theta) - e * cos(delta_theta)
-
-        #Computo de elementos de la diagonal
-        else:
-            v_i = v_and_theta[i]
-            theta_i = v_and_theta[n + i]
-            G_ii = mat_admitancia[i,i].real
-            B_ii = mat_admitancia[i,i].imag
-
-            P_i = 0
-            Q_i = 0
-            # dP/dtheta
-            J[i, j] = - Q_i - B_ii * (v_i**2)
             # dP/dV
-            J[i, n + j] = P_i / v_i + G_ii * v_i
-            # dQ/dtheta
-            J[n + i, j] = P_i - G_ii * (v_i**2)
+            J[i_J, (n - 1) + j_J] = d * cos(delta_theta) + e * sin(delta_theta)
             # dQ/dV
-            J[n + i, n + j] = Q_i / v_i - B_ii * v_i
+            J[(n - 1) + i_J, (n - 1) + j_J] = d * sin(delta_theta) - e * cos(delta_theta)
 
-print(J)
+        # Elementos de la diagonal
+        # ---------------------------------------------------------------------------------------------
+        else:
+            v_i = v_and_theta[i, 0]
+            G_ii = mat_admitancia[i, i].real
+            B_ii = mat_admitancia[i, i].imag
+
+            P_i = P_Q[i_J]
+            Q_i = P_Q[(n - 1) + i_J]
+            # dP/dtheta
+            J[i_J, j_J] = - Q_i - B_ii * (v_i ** 2)
+            # dP/dV
+            J[i_J, (n - 1) + j_J] = P_i / v_i + G_ii * v_i
+            # dQ/dtheta
+            J[(n - 1 + i_J, j_J] = P_i - G_ii * (v_i ** 2)
+            # dQ/dV
+            J[(n - 1) + i_J, (n - 1) + j_J] = Q_i / v_i - B_ii * v_i
+
+
+
+if __name__ == '__main__':
+    import IEEE_cdf as cdf
+    n, mat, load, generation, voltage_phase = cdf.read("ieee14cdf.txt")
+    # Vector de 2 veces la cantidad de nodos. Una vez para la tension y otra para la fase
+    v_and_theta = np.ones((2 * n), dtype=np.float64)
+
+    J = jacobian(mat, v_and_theta)
+
+    print(J)
